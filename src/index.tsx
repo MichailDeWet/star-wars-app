6e5c1016e9c168e94045c647a98bdff1f9d51d39
Michail De Wet <132671140+MichyMichs@users.noreply.github.com>
2025-02-16 12:58:07 +0200

    Disable Strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
@@ -9,11 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
